645068e13d07feb93ce98528c8733e3ff36a4f5a
Mohammad Dargahi <mamad.drg@gmail.com>
2025-08-30 18:10:07 +0330

    Add all the mutations

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -4,26 +4,68 @@ import type { AppContextModel } from '../models';
 
 export const Mutation = {
   createUser: async (parent, args, ctx: AppContextModel, info) => {
-    if (args.password.length < 8) {
+    const { data } = args;
+    if (data.password.length < 8) {
       throw new Error('Password should be at least 8 characters');
     }
     const isEmailTaken = await ctx.dbClient.user.findFirst({ 
-      where: { email: args.email }
+      where: { email: data.email }
     });
     if (isEmailTaken) {
       throw new Error("Email is already in use");
     }
-
     const salt = await bcrypt.genSalt();
-    const hashedPass = bcrypt.hashSync(args.password, salt);
+    const hashedPass = await bcrypt.hash(data.password, salt);
     const userData = {
-      name: args.name,
-      email: args.email,
+      ...data,
       password: hashedPass,
-      bio: args.bio ?? null
     }
-
     const result = await ctx.dbClient.user.create({ data: userData });
     return result;
-  }
+  },
+  createPost: async (parent, args, ctx: AppContextModel, info) => {
+    const { data } = args;
+    const isUserAvailable = ctx.dbClient.user.findFirst({
+      where: { id: data.authorId }
+    });
+    if (!isUserAvailable) {
+      throw new Error('User is not defined');
+    }
+    const result = await ctx.dbClient.post.create({ data });
+    return result;
+  },
+  createComment: async (parent, args, ctx: AppContextModel, info) => {
+    const { data } = args;
+    const isUserAvailable = ctx.dbClient.user.findFirst({
+      where: { id: data.authorId }
+    });
+    if (!isUserAvailable) {
+      throw new Error('User is not defined');
+    }
+    const isPostAvailable = ctx.dbClient.post.findFirst({
+      where: { id: data.postId }
+    });
+    if (!isPostAvailable) {
+      throw new Error('Post is not defined');
+    }
+    const result = await ctx.dbClient.comment.create({ data });
+    return result;
+  },
+  createLike: async (parent, args, ctx: AppContextModel, info) => {
+    const { data } = args;
+    const isUserAvailable = ctx.dbClient.user.findFirst({
+      where: { id: data.userId }
+    });
+    if (!isUserAvailable) {
+      throw new Error('User is not defined');
+    }
+    const isPostAvailable = ctx.dbClient.post.findFirst({
+      where: { id: data.postId }
+    });
+    if (!isPostAvailable) {
+      throw new Error('Post is not defined');
+    }
+    const result = await ctx.dbClient.like.create({ data });
+    return result;
+  },
 }
