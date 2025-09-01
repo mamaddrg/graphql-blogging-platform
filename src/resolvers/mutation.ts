2515f5ee0a56bd6419b40ea60106f3cd6943ffd1
Mohammad Dargahi <mamad.drg@gmail.com>
2025-09-01 17:03:18 +0330

    Add all the possible mutations for update and delete

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -39,7 +39,7 @@ export const Mutation = {
       ...args.data,
       authorId: authData.userId
     }
-    const isUserAvailable = ctx.dbClient.user.findUnique({
+    const isUserAvailable = await ctx.dbClient.user.findUnique({
       where: { id: authData.userId }
     });
     if (!isUserAvailable) {
@@ -110,5 +110,154 @@ export const Mutation = {
       token,
       user: userData
     }
+  },
+  updateUser: async (parent, args, ctx: AppContextModel, info): Promise<UserModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    const { data } = args;
+
+    if (data.email) {
+      const isEmailTaken = await ctx.dbClient.user.findUnique({ 
+        where: { email: data.email }
+      });
+      if (isEmailTaken) {
+        throw new Error('Email is already in use');
+      }
+    }
+    if (data.password) {
+      if (data.password.length < 8) {
+        throw new Error('Password should be at least 8 characters');
+      }
+      const hashedPass = await hashPassword(data.password);
+      data.password = hashedPass;
+    }
+
+    const result = await ctx.dbClient.user.update({ 
+      data, 
+      where: { id: authData.userId } 
+    });
+    return result;
+  },
+  updatePost: async (parent, args, ctx: AppContextModel, info): Promise<PostModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    const { id, data } = args;
+
+    const postRecord = await ctx.dbClient.post.findUnique({ where: { id } });
+    if (!postRecord) {
+      throw new Error('Post not found');
+    }
+    if (postRecord.authorId !== authData.userId) {
+      throw new Error("You're not authorized to perform this action");
+    }
+
+    let shouldDropDependencies = false;
+    if (
+      data.published != null &&
+      data.published === false &&
+      postRecord.published === true
+    ) {
+      // user want's to un-publish the post, so all the comments and likes should be dropped
+      shouldDropDependencies = true;
+    }
+
+    let result: PostModel;
+    if (shouldDropDependencies) {
+      // TODO => is there a better way to handle this transaction?
+      [result] = await ctx.dbClient.$transaction([
+        ctx.dbClient.post.update({
+          where: { id: postRecord.id },
+          data
+        }),
+        ctx.dbClient.comment.deleteMany({
+          where: { postId: postRecord.id }
+        }),
+        ctx.dbClient.like.deleteMany({
+          where: { postId: postRecord.id }
+        })
+      ])
+    } else {
+      result = await ctx.dbClient.post.update({
+        where: { id: postRecord.id },
+        data
+      });
+    }
+
+    return result;
+  },
+  updateComment: async (parent, args, ctx: AppContextModel, info): Promise<CommentModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    const { id, data } = args;
+    const commentRecord = await ctx.dbClient.comment.findUnique({ where: { id } });
+    if (!commentRecord) {
+      throw new Error('Comment not found');
+    }
+    if (commentRecord.authorId !== authData.userId) {
+      throw new Error("You're not authorized to perform this action");
+    }
+    const result = await ctx.dbClient.comment.update({
+      where: { id },
+      data 
+    });
+    return result;
+  },
+  deleteUser: async (parent, args, ctx: AppContextModel, info): Promise<UserModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    let result: UserModel;
+    // TODO => is there a better way to handle transactions?
+    // TODO => should we just rely on delete user, and the dependency deletion based on primary key?
+    await ctx.dbClient.$transaction(async (tx) => {
+      await tx.like.deleteMany({ where: { userId: authData.userId } });
+      await tx.comment.deleteMany({ where: { authorId: authData.userId } });
+      await tx.post.deleteMany({ where: { authorId: authData.userId } });
+      result = await tx.user.delete({ where: { id: authData.userId } });
+    });
+    return result;
+  },
+  deletePost: async (parent, args, ctx: AppContextModel, info): Promise<PostModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    const { id } = args;
+
+    const postRecord = await ctx.dbClient.post.findUnique({ where: { id } });
+    if (!postRecord) {
+      throw new Error('Post not found');
+    }
+    if (postRecord.authorId !== authData.userId) {
+      throw new Error("You're not authorized to perform this action");
+    }
+
+    let result: PostModel;
+    // TODO => is there a better way to handle transactions?
+    // TODO => should we just rely on delete user, and the dependency deletion based on primary key?
+    await ctx.dbClient.$transaction(async (tx) => {
+      await tx.like.deleteMany({ where: { postId: id } });
+      await tx.comment.deleteMany({ where: { postId: id } });
+      result = await tx.post.delete({ where: { id } });
+    });
+    return result;
+  },
+  deleteComment: async (parent, args, ctx: AppContextModel, info): Promise<CommentModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    const { id } = args;
+    const commentRecord = await ctx.dbClient.comment.findUnique({ where: { id } });
+    if (!commentRecord) {
+      throw new Error('Comment not found');
+    }
+    if (commentRecord.authorId !== authData.userId) {
+      throw new Error("You're not authorized to perform this action");
+    }
+    const result = await ctx.dbClient.comment.delete({ where: { id } });
+    return result;
+  },
+  deleteLike: async (parent, args, ctx: AppContextModel, info): Promise<LikeModel> => {
+    const authData = extractJwtAuth(ctx.request);
+    const { id } = args;
+    const likeRecord = await ctx.dbClient.like.findUnique({ where: { id } });
+    if (!likeRecord) {
+      throw new Error('Comment not found');
+    }
+    if (likeRecord.userId !== authData.userId) {
+      throw new Error("You're not authorized to perform this action");
+    }
+    const result = await ctx.dbClient.like.delete({ where: { id } });
+    return result;
   },
 }
